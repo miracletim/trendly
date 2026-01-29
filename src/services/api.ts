023cf6d910e6973dd29562517fd4f7ce676bc1be
Miracle Timothy <miracletimothyofficial@gmail.com>
2026-01-29 14:12:57 +0300

    preped for production

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,6 @@
 import type { ApiResponse } from "../types";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL_PROD;
 
 export async function fetchTopVideos(regionCode: string): Promise<ApiResponse> {
   try {
